ae1d1be5f598e22beb47ff99bb842ae945c10210
Yanis1453 <peiayanis7@gmail.com>
2025-03-05 15:27:38 +0200

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -1,20 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
-
 int function(int x){
-	x=x-2;
-	x=function(x);
-	return x;
+    if (x <= 0) {
+        return 0; 
+    }
+    x = x - 2;
+    x = function(x);
+    return x;
 }
-
 int main(){
-	int a=30, i=0;
-	
-	while (a>0){
-		a=function(a);
-		i++;
-	}
-	printf("%d", i);
-	return 0;	
+    int a = 30, i = 0;
+    while (a > 0){
+        a = function(a);
+        i++;
+    }
+    printf("%d", i);
+    return 0;    
 }
 
